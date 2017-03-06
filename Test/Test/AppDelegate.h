1995188c99f5fb5bdf2386d850f38a213f4bc05e
李合意 <yi@liheyideMacBook-Pro.local>
2017-03-06 16:57:53 +0800

    no message

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 李合意 on 2017/3/6.
+//  Copyright © 2017年 李合意. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 李合意 on 2017/3/6.
+//  Copyright © 2017年 李合意. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
